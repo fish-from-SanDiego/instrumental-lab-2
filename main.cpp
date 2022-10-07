2c4ecfad6ee1b414b58e360337e65c2a931089d5
Maksim Petrov <petrov.maksim5@mail.ru>
2022-10-07 19:47:02 +0300

    code refactoring in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@
 
 int main() {
     std::queue<int> q;
-    for (int i = 0; i < 10; i++){
+    for (int i = 0; i < 10; i++) {
         q.push(i);
     }
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++) {
         int t = q.front();
         q.pop();
         std::cout << t << ' ';
